ca7baaf914031463a00fd758cf444d87e34c3d6d
ZhdanovAndrei <k4ty224@mail.ru>
2024-11-08 18:02:09 +0300

    Update Transportation.cpp

--- Transportation.cpp
+++ Transportation.cpp
@@ -9,9 +9,45 @@ struct Allocation {
     int amount;
 };
 
+void printTable(vector<int> supply, vector<int> demand, const vector<vector<int>>& cost){
+    int inputTable[4][5];
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 5; j++){
+
+            if (i == 3 && j == 4){
+                inputTable[i][j] = 0;
+            }
+            else if (i < 3 && j < 4){
+                inputTable[i][j] = cost[i][j]; 
+            }
+            else if (i == 3){
+            
+                inputTable[i][j] = demand[j];
+            }
+            else if (j == 4){
+                inputTable[i][j] = supply[i];
+            }
+        }
+    }
+    cout << "input Table:" << endl;
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 5; j++){
+            cout << inputTable[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 bool isBalanced(const vector<int>& supply, const vector<int>& demand) {
-   
+   int sumSupply = 0;
+   int sumDemand = 0;
+   for (int i = 0; i < supply.size(); i++){
+    sumSupply += supply[i];
+   }
+   for (int i = 0; i < demand.size(); i++){
+    sumDemand += demand[i];
+   }
+   return (sumDemand == sumSupply);
 }
 
 vector<Allocation> northWestCorner(vector<int> supply, vector<int> demand, const vector<vector<int>>& cost) {
@@ -28,6 +64,7 @@ vector<Allocation> vogelApproximation(vector<int> supply, vector<int> demand, co
 
 vector<Allocation> russellApproximation(vector<int> supply, vector<int> demand, const vector<vector<int>>& cost) {
     vector<Allocation> allocations;
+
     return allocations;
 }
 
@@ -54,6 +91,8 @@ int main() {
         return 0;
     }
     
+    printTable(supply, demand, cost);
+
     cout << "North-West Corner Method:" << endl;
     auto nwAllocations = northWestCorner(supply, demand, cost);
     printAllocations(nwAllocations);
